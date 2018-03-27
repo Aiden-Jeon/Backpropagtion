c5cd31f7e7fb6a59b65ab958210439c693fd4690
JongSeobJ <idyo92@Naver.com>
2018-03-27 16:11:38 +0900

    function name and code modified

--- back_3_n_more.py
+++ back_3_n_more.py
@@ -41,8 +41,6 @@ class optimizer:
     def loss(self,real,predict,loss_type='mse'):
         if loss_type.lower() == 'mse':
             return np.array(predict) - np.array(real)
-        if loss_type.lower() == 'cross_entropy':
-            return np.array(real) * np.array(predict)
         
     def accuracy(self,pred,real):
         count = 0 
@@ -53,7 +51,7 @@ class optimizer:
                 count += 1
         return count/len(pred)
 
-    def SGD(self,x,y,layer,loss_type,lr=0.01,epoch=10):
+    def GradientDescent(self,x,y,layer,loss_type,lr=0.01,epoch=10):
         def layer_cal(train_data,weight,bias):
             layer[0] = layers().layer(train_data,weight[0],bias[0])
             for la in range(1,len(weight)):
@@ -94,24 +92,65 @@ class optimizer:
                 b_update[idx] = b_upd
             return w_update, b_update
         
-        ## 전체 data set 
+
         out,out_back,weight,bias,active,net = dictionary(layer)
         for ep in range(epoch):
+            w_log = dict(); b_log = dict()
+            for i in range(len(weight)):
+                w_log[i] = weight[i] * 0.0
+                b_log[i] = bias[i] * 0.0
             for iteration in range(len(x)):
                 layer = layer_cal(x[iteration],weight,bias)
                 out,out_back,_,_,active,net = dictionary(layer)
                 out_net = out_net_cal(out,out_back)
                 loss_val = self.loss(y[iteration],out[len(out)-1],loss_type)
                 w_update, b_update = training(loss_val,out_net,out_back,weight,bias)
-                ## update
                 for i in range(len(weight)):
-                    weight[i] = weight[i] - lr*w_update[i]
-                    bias[i] = bias[i] - lr*b_update[i]
-
-            if ep % 10 == 0:
+                    w_log[i] += w_update[i]
+                    b_log[i] += b_update[i]
+            for i in range(len(weight)):
+                w_log[i] = w_log[i]/(len(x)+1)
+                weight[i] = weight[i] - lr*w_log[i]
+                b_log[i] = b_log[i]/(len(x)+1)
+                bias[i] = bias[i] - lr*bias[i]
+            
+            if ep % 1000 == 0:
                 temp = activation().forward(np.array(x).dot(weight[0])+bias[0],active[0])
                 for i in range(1,len(weight)):
                     temp =  activation().forward(temp.dot(weight[i])+bias[i],active[i])
                 print(self.accuracy(temp,y))
                 
         return dict(weight=weight,bias=bias)
+
+#example
+import pandas as pd
+
+data = pd.read_csv('mnist_train_100.csv')
+
+y_data = data.values[:,0]
+x_data = data.values[:,1:]
+y_data = pd.get_dummies(y_data).values
+
+W1 =layers().weight([784,40])
+W2 = layers().weight([40,20])
+W3 = layers().weight([20,10])
+x_t =layers().train_data(x_data.shape)
+layer1 = layers().layer(x_t,W1,np.zeros(40))
+layer2 = layers().layer(layer1,W2,np.zeros(20))
+layer3 = layers().layer(layer2,W3,np.zeros(10))
+layer = [layer1,layer2,layer3]
+
+optimizer().GradientDescent(x_data,y_data,layer,lr=0.1,loss_type='mse',epoch=10000)
+#result
+'''
+0.04040404040404041
+0.5454545454545454
+0.6868686868686869
+0.7373737373737373
+0.8282828282828283
+0.8787878787878788
+0.8888888888888888
+0.9090909090909091
+0.9393939393939394
+0.9696969696969697
+'''
